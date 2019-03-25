d7b5831d0782d84d0b479ea4b9a2c1da39823e8f
Dennys Augustus <dennysaug@gmail.com>
2019-03-25 09:50:23 -0300

    Update

--- monitora_libra.sh
+++ monitora_libra.sh
@@ -14,16 +14,17 @@
 #depois de dar permissão, executa: ./monitora_libra.sh
 #código feito e testando em ambiente Linux.
 
+printf "\n####### MONITORA COTAÇÃO DA LIBRA NO TRANSFERWISE #######\n"
 if [ "$1" == "" ] 
-then
-	printf "####### MONITORA COTAÇÃO DA LIBRA NO TRANSFERWISE #######\n"
+then	
 	printf "# Deve informar a cotação em real que deseja\n"
 	printf "# Ex.: $0 4.75\n\n"
 else
+			
 	while [ 1 ]
 	do
 
-	curl -o cotacao -sS -X GET "https://api.sandbox.transferwise.tech/v1/rates?source=GBP&target=BRL" -H "Authorization: Bearer <token>" 
+	curl -o cotacao -sS -X GET "https://api.sandbox.transferwise.tech/v1/rates?source=GBP&target=BRL" -H "Authorization: Bearer b08cd38b-9114-4f83-9ab3-c654bc1531aa" 
 
 	cat cotacao | cut -d ',' -f1 | cut -d ':' -f 2 | sed 's/...$//' > libra
 	if [ $((echo "scale=4; $(<libra) <= $1") | bc -l) == 1 ] 
